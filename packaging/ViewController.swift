ce91b781da8828bd4a6a1b1382c72f7153302cff
abu <2261983469@qq.com>
2017-07-31 11:54:11 +0800

    提交项目

---
+++ packaging/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  packaging
+//
+//  Created by MyLifeIsNotLost on 2017/7/31.
+//  Copyright © 2017年 MyLifeIsNotLost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.yellow
+            
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
